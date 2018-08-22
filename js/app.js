7174460f1413be84ce488120af494dd1d245bf0f
ethan375 <ethanebel1@gmail.com>
2018-08-22 14:35:03 -0500

    front page done exluding logo. building out views

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 console.log("files working");
+const app = angular.module('Undercover', ['ngRoute']);
+
+  app.config(function($routeProvider){
+    
+    $routeProvider
+    .when( '/arcadia',{
+      templateUrl:'./views/arcadia.html'
+    })
+    .when('/aluminumpergola',{
+      templateUrl: './views/alumpergola.html' 
+    })
+    .when('/commercial',{
+      templateUrl:'./views/commercial.html'
+    })
+    .when('/heaters',{
+      templateUrl:'./views/heaters.html'
+    })
+    .when('/home', {
+      templateUrl: './views/home.html'
+    })
+    .when('residential',{
+      templateUrl:'./views/residential.html'
+    })
+    .when('/retractableroofs',{
+      templateUrl:'./views/retracroofs.html'
+    })
+    .when('/rectratableawnings',{
+      templateUrl:'./views/retractable.html'
+    })
+    .when('/screens',{
+      templateUrl:'./views/screens.html'
+    })
+    .otherwise({
+      templateUrl: './views/home.html'
+    })
+  });
